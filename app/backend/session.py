cb57934e196e9e2a65c99dfecdbdb00b4f06a175
JhoanSFranco <jhfrancor@unal.edu.co>
2025-07-21 23:00:24 -0500

    Docker

--- app/backend/session.py
+++ app/backend/session.py
@@ -21,7 +21,9 @@ print(config)
 
 
 # Crear engine
-engine = create_engine(settings.sqlalchemy_database_uri) # Gestiona las conexion en la app
+# engine = create_engine(settings.sqlalchemy_database_uri) # Gestiona las conexion en la app
+engine = create_engine("mysql+pymysql://root:1234@host.docker.internal:3306/cursos_db") # Gestiona las conexion en la app
+
 
 # Crear sesión
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine) # Crea una sección personalizada cada vez que la base de datos necesita realziar una operacion de CONSULTA, INSERTAR, ACTUALIZAR
